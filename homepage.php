918f0d7713ef1c167f03ba005684e7eaff54f772
dondichon <fabio.bally@students.fhnw.ch>
2014-10-06 17:09:57 +0200

    php File hinzu

---
+++ homepage.php
@@ -0,0 +1,79 @@
+<?php
+
+
+?>
+<html>
+    <head>
+        <link rel="stylesheet" href="mycss.css" type="text/css">
+        <title>Online-Verwaltungstool f&uuml;r Mehrfamilienhaus</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body class="content">
+
+        
+        <ul class="navbar1">
+            <li class="topmenu"> <a href="#" >Menupunkt1 </a>
+                <ul>
+                    <li class="submenu"><a href="#">Option1</a></li>
+                    <li class="submenu"><a href="#">Option2</a></li>
+                    <li class="submenu"><a href="#">Option3</a></li>
+                </ul>
+            </li>
+
+            <li class="topmenu"> <a href="#" >Menupunkt2 </a>
+                <ul>
+                    <li class="submenu"><a href="#">Option1</a></li>
+                    <li class="submenu"><a href="#">Option2</a></li>
+                    <li class="submenu"><a href="#">Option3</a></li>
+                </ul>
+            </li>
+
+            <li class="topmenu"> <a href="#" >Menupunkt3 </a>
+                <ul>
+                    <li class="submenu"><a href="#">Option1</a></li>
+                    <li class="submenu"><a href="#">Option2</a></li>
+                    <li class="submenu"><a href="#">Option3</a></li>
+                </ul>
+            </li>
+
+            <li class="topmenu"> <a href="#" > Logout </a>
+                
+            </li>
+
+        </ul>
+
+
+
+
+        <img src="house2.jpg" class="picture2">
+
+
+        <ul class="navbar2">
+            <li> Menuname1</li>
+            <li> Option1</li>
+            <li> Option2</li>
+            <li> Option3</li>
+            <li> Option4</li>
+        </ul>
+
+        <div class ="main">
+            MAIN 
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+
+        </div>
+
+        <div class="foot">
+            Web-Engineering Project, &copy;Fabio Bally, Aurelia Erhardt, Marco Mangold
+        </div>
+
+    </body>
+</html>
