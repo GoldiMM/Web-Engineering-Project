246046b10992313aec2fd1643f538761fc642ffe
GoldiMM <marco.mangold.marco@gmail.com>
2014-10-10 15:00:23 +0200

    login und homepage funktioniert mit Erkennung vom User.

--- homepage.php
+++ homepage.php
@@ -1,94 +1,65 @@
 <?php
-    session_start();
 
+// vorhandene Session aufnehmen
+session_start();
+
+if (isset($_POST['nickname']) AND isset($_POST['password'])) {
+    if ($_POST['nickname'] == "admin" AND $_POST['password'] == "root") {
+        // zwei Session-Variablen setzen
+        $_SESSION['nickname'] = $_POST['nickname'];
+        $_SESSION['eingeloggt'] = true;
+
+        echo '
+  <!-- Home-Page -->
+  <html>
+  <head> 
+  <title> Online-Verwaltungstool </title>
+  <link rel="stylesheet" href="mycss.css" type="text/css">  
+  </head>
+  ';
+        //Nickname zwischenspeichern
+        $nick = $_POST['nickname'];
+
+
+        echo "
+      <form action=\"login.php\" method=\"POST\">
+      <body>
+  <header> 
+  <p> Sie sind eingeloggt als $nick 
+  <input type=\"submit\" value=\"Logout\">
+  </p>
+  <img class=\"picture2\" src=\"house2.jpg\" > 
+  </header>
+";
+
+        include('nav.inc.php');
+        include('aside.inc.php');
+
+        echo '
+<article>
+
+<p> Hier kommen die Ausfuehrungen mit Formularen usw. !!!
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+
+</article>
+';
+
+        include('footer.inc.php');
+
+        echo "
+    </form>
+</body>
+</html> 
+  ";
+    } else {
+        echo "ung&uuml;ltige Eingabe!";
+        $_SESSION['eingeloggt'] = false;
+        echo "<a href=\"login.php\"> zur&uuml;ck zur Login-Seite </a>";
+    }
+}
 ?>
-<html>
-    <head>
-        <link rel="stylesheet" href="mycss.css" type="text/css">
-        <title>Online-Verwaltungstool f&uuml;r Mehrfamilienhaus</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    </head>
-    <body class="content">
-        <?php
-        if (isset($_GET['nickname']) AND isset($_GET['password'])) {
-            if($_GET['nickname']=="admin" AND
-                    $_GET['password']=="root")
-            {
-                $_SESSION['eingeloggt']=true;
-                echo "<br/><a href=\"homepage.php\"> Hallo MR. X</a>";
-            }
-            else{
-                $_SESSION['eingeloggt']=false;
-                header("Location:login.php");
-            }
-        }
-        
-        ?>
-
-        
-        <ul class="navbar1">
-            <li class="topmenu"> <a href="#" >Menupunkt1 </a>
-                <ul>
-                    <li class="submenu"><a href="#">Option1</a></li>
-                    <li class="submenu"><a href="#">Option2</a></li>
-                    <li class="submenu"><a href="#">Option3</a></li>
-                </ul>
-            </li>
-
-            <li class="topmenu"> <a href="#" >Menupunkt2 </a>
-                <ul>
-                    <li class="submenu"><a href="#">Option1</a></li>
-                    <li class="submenu"><a href="#">Option2</a></li>
-                    <li class="submenu"><a href="#">Option3</a></li>
-                </ul>
-            </li>
-
-            <li class="topmenu"> <a href="#" >Menupunkt3 </a>
-                <ul>
-                    <li class="submenu"><a href="#">Option1</a></li>
-                    <li class="submenu"><a href="#">Option2</a></li>
-                    <li class="submenu"><a href="#">Option3</a></li>
-                </ul>
-            </li>
-
-            <li class="topmenu"> <a href="#" > Logout </a>
-                
-            </li>
-
-        </ul>
-
-
-
-
-        <img src="house2.jpg" class="picture2">
-
-
-        <ul class="navbar2">
-            <li> Menuname1</li>
-            <li> Option1</li>
-            <li> Option2</li>
-            <li> Option3</li>
-            <li> Option4</li>
-        </ul>
-
-        <div class ="main">
-            MAIN 
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-
-        </div>
-
-        <div class="foot">
-            Web-Engineering Project, &copy;Fabio Bally, Aurelia Erhardt, Marco Mangold
-        </div>
-
-    </body>
-</html>
--- login.php
+++ login.php
@@ -1,50 +1,58 @@
 <?php
-       session_start();
-        // Session löschen
-        session_unset();
-        session_destroy();
-        //Session-Array wird gelöscht
-        unset($_SESSION);
-        ?>
+//Eine Session starten
+session_start();
 
+// Löschen aller Session-Variablen, somit können wir ein 
+// Logout auf diese Seite lenken
+$_SESSION = array();
 
+// Falls die Session gelöscht werden soll, löschen Sie auch das Session-Cookie.
+// Achtung: Damit wird die Session gelöscht, nicht nur die Session-Daten!
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]
+    );
+}
+
+// Zum Schluß, löschen aller Daten der Session.
+session_destroy();
+?>
+
+<!-- Login-Page -->
 <html>
     <head>
-        <link rel="stylesheet" href="mycss.css" type="text/css">
-        <title>Online-Verwaltungstool f&uuml;r Mehrfamilienhaus</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Online-Verwaltungstool</title> 
+        <link rel="stylesheet" href="mycss.css" type="text/css">  
     </head>
-    
+
     <body>
-             
-      
-       
-       
-        <div class ="login" >
-            <form action="homepage.php" method="GET">
-            <h1> Online-Verwaltungstool f&uuml;r Mehrfamilienhaus </h1>
-            <img src="house.jpg" class="picture1"> <br/> 
-            <p>
-            <table   align="center" >
-                <tr>
-                    <th colspan="2" align="left"> <h2>  Login </h2> </th>
-                </tr>
-                <tr>
-                    <td> Benutzername </td>
-                    <td> <input name="nickname" type="text"  size="15"> </td>
-                </tr>
-                <tr>
-                    <td> Passwort: </td>
-                    <td> <input name="password" type="password"  size="15"> </td>
-                </tr>
-                <tr>
-                    <td colspan="2" align="right"> <input type="submit" value="Login"> </td>
-                </tr>
-            </table>
-            </form>
-        </p>
-        </div>
-        
-    </body>
+        <form action="homepage.php" method="POST">
+            <header> 
+                <p>
+                    Benutzername <input name="nickname" type="text" size="15"> 
+                    Passwort: <input name="password" type="password" size="15"> 
+                    <input type="submit" value="Login">
+                </p> 
+                <img class="picture2" src="house2.jpg" > 
+
+            </header>
+
+            <nav> &nbsp; </nav>
+
+            <aside> &nbsp; </aside>
+
+            <article>
+
+                <h1 align="center"> Herzlich Willkommen auf der Seite Online-Verwaltungstool f&uuml;r Mehrfamilienhaus </h1>
+                <p align="center"> <img  src="house.jpg" align="center"> <br/>
+                    Bitte loggen Sie sich oben rechts ein! </p>
+
+            </article>
+
+            <?php
+            include('footer.inc.php');
+            ?>        
+
+        </form>
+    <body>
 </html>
