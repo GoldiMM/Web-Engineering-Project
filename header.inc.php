7aa013c3884e582d2be1415a62e219dffa96036c
dondichon <fabio.bally@students.fhnw.ch>
2014-10-13 17:46:13 +0200

    korrektur variablen

--- header.inc.php
+++ header.inc.php
@@ -3,9 +3,9 @@
 echo '
 <header> 
 <p>
-Benutzername <input type="text" size="15"> 
-Passwort: <input type="password" size="15"> 
-<input type="submit" value="Login">
+Benutzername <input type="text" name="nickname" size="15"> 
+Passwort: <input type="password" name="password" size="15"> 
+<input type="submit" value="einloggen">
 </p> 
 <img class="picture2" src="house2.jpg" > 
 </header>
