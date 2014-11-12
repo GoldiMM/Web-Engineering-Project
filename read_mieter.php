55fdb147648d570f5a2dbe3066273cb39c786c56
aureliailerua <aureliailerua@gmail.com>
2014-11-12 13:19:45 +0100

    Ausgabe der Mieterliste als includable file

---
+++ read_mieter.php
@@ -0,0 +1,23 @@
+<?php
+	include ('db_Cando.inc.php');
+	// Create connection
+
+	echo "<article>";
+		// _________________________Feedback Resultat Ausgabe ___________________
+			echo ("<h3>Aktuelle Mieterliste </h3> <br>");
+
+			$sql = "SELECT Mieter_ID, Anrede, Vorname, Nachname FROM Mieter ORDER BY Mieter_ID DESC";
+			$result = $conn->query($sql);
+				if ($result->num_rows > 0) {
+				    while($row = $result->fetch_assoc()) {
+				        echo 	"Mieter " 		. $row["Mieter_ID"].
+						        "   : "			. $row["Anrede"]. 
+						        "     "			. $row["Vorname"]. 
+						        "    " 			. $row["Nachname"]. "<br>";
+					}
+				} 
+				else {
+					    echo "0 results";
+				}
+	echo "</article>";
+	?>
