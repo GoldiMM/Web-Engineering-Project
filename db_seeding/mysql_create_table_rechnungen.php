95aff97d5ee61dac763a0e77fcb05d63f5ca65b9
aureliailerua <aureliailerua@gmail.com>
2014-11-18 10:05:49 +0100

    create Table für Rechnungen

---
+++ db_seeding/mysql_create_table_rechnungen.php
@@ -0,0 +1,31 @@
+<?php
+
+$db_host = "localhost";
+$db_username = "Cando";
+$db_pass = "yes123";
+$db_name = "Hausverwaltung";
+
+// Create connection
+$conn = new mysqli($db_host, $db_username, $db_pass, $db_name);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// sql to create table
+$sql = "CREATE TABLE IF NOT EXISTS Rechnungen (
+	Rechnungs_ID INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+	Rechnungsdatum DATE,
+	Kategorie ENUM('Reparaturen','Öl','Wasser','Strom','Hauswart','',''),
+	Betrag FLOAT
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo " table created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+
+$conn->close();
+?>  
