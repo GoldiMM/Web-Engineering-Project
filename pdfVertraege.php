b25609b9cf569f9ca9115ebb5432072c3653b624
dondichon <fabio.bally@students.fhnw.ch>
2014-11-26 11:01:33 +0100

    PDF updatev3

---
+++ pdfVertraege.php
@@ -0,0 +1,31 @@
+<?php
+require('mysql_table.php');
+
+class PDF extends PDF_MySQL_Table
+{
+function Header()
+{
+	//Title
+	$this->SetFont('Arial','',18);
+	$this->Cell(0,6,'Rechnungsuebersicht',0,1,'C');
+	$this->Ln(10);
+	//Ensure table header is output
+	parent::Header();
+}
+}
+
+//Connect to database
+mysql_connect('localhost','Cando','yes123');
+mysql_select_db('hausverwaltung');
+
+$pdf=new PDF();
+$pdf->AddPage();
+$mieter = ('SELECT * FROM mietvertraege ');
+//First table: put all columns automatically
+$pdf->Table($mieter);
+
+
+
+$pdf->Output($downloadfilename.".pdf"); 
+header('Location: '.$downloadfilename.".pdf");
+?>
---
+++ pdfrech_1.php
@@ -0,0 +1,44 @@
+<?php
+require('mysql_table.php');
+
+class PDF extends PDF_MySQL_Table
+{
+function Header()
+{
+	//Title
+	$this->SetFont('Arial','',18);
+	$this->Cell(0,6,'Rechnungsuebersicht',0,1,'C');
+	$this->Ln(10);
+	//Ensure table header is output
+	parent::Header();
+}
+}
+
+//Connect to database
+mysql_connect('localhost','Cando','yes123');
+mysql_select_db('hausverwaltung');
+
+$pdf=new PDF();
+$pdf->AddPage();
+$wasser = ('SELECT * FROM rechnungen ');
+//First table: put all columns automatically
+$pdf->Table($wasser);
+
+
+//$pdf->AddPage();
+//Second table: specify 3 columns
+//$pdf->AddCol('Kategorie',40,'','C');
+//$pdf->AddCol('Betrag',40,'','C');
+//$prop=array('HeaderColor'=>array(255,150,100),
+			//'color1'=>array(210,245,255),
+			//'color2'=>array(255,255,210),
+			//'padding'=>2);
+//$pdf->Table('SELECT Kategorie, SUM(Betrag) FROM `rechnungen` WHERE Kategorie =\'Wasser\'',$prop);
+
+
+//$pdf->Output("C:\Users\John\Desktop/somename.pdf",'F'); 
+
+
+$pdf->Output($downloadfilename.".pdf"); 
+header('Location: '.$downloadfilename.".pdf");
+?>
