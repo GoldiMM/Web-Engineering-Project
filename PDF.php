e435ec76215718cfc6f56666cfcf1061db45f59f
dondichon <fabio.bally@students.fhnw.ch>
2014-11-26 11:57:41 +0100

    PDF updatev4

--- PDF.php
+++ PDF.php
@@ -1,41 +1,17 @@
 <?php
-require('mysql_table.php');
-
-class PDF extends PDF_MySQL_Table
-{
-function Header()
-{
-	//Title
-	$this->SetFont('Arial','',18);
-	$this->Cell(0,6,'Mieteranzeige',0,1,'C');
-	$this->Ln(10);
-	//Ensure table header is output
-	parent::Header();
-}
-}
-
-//Connect to database
+//include fpdf
+require('PDF_Generator\\fpdf.php');
+//include DB
 mysql_connect('localhost','Cando','yes123');
 mysql_select_db('hausverwaltung');
 
-$pdf=new PDF();
+//globals
+$wasser = (double)('SELECT Kategorie, SUM(Betrag) FROM `rechnungen` WHERE Kategorie =\'Wasser\'');
+//create PDF
+$pdf = new FPDF();
 $pdf->AddPage();
-//First table: put all columns automatically
-$pdf->Table('SELECT Anrede, Vorname, Nachname FROM `mieter`');
-//$pdf->AddPage();
-//Second table: specify 3 columns
-//$pdf->AddCol('Anrede',40,'','C');
-//$pdf->AddCol('Vorname',40,'Vorname','C');
-//$pdf->AddCol('Nachname',40,'Nachname','C');
-//$prop=array('HeaderColor'=>array(255,150,100),
-			//'color1'=>array(210,245,255),
-			//'color2'=>array(255,255,210),
-			//'padding'=>2);
-//$pdf->Table('SELECT * FROM `mieter` limit 0,10',$prop);
-
-//$pdf->Output("C:\Users\John\Desktop/somename.pdf",'F'); 
-
-
-$pdf->Output($downloadfilename.".pdf"); 
-header('Location: '.$downloadfilename.".pdf");
+$pdf->SetFont('Arial','B',16);
+$pdf->Cell(40,10,$wasser);
+$pdf->Cell(40,10,'Hello World !');
+$pdf->Output();
 ?>
