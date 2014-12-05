d72345211423788658c685767413ad9b4e375e9a
GoldiMM <marco.mangold.marco@gmail.com>
2014-12-05 11:23:32 +0100

    Homepage update (Infos auf der Webseite aktualisiert --> push to Webserver done)

--- homepage.article.inc.php
+++ homepage.article.inc.php
@@ -15,24 +15,34 @@ echo "
         Ausgangspunkt der Webseite. <br/>
         <br/>
         <b> Mieter </b> <br/>
-        (kurze Erklarung)
+        Unter dem Men&uuml;punkt Mieter hat es eine aktuelle Mieterliste mit den wichtigsten Informationen &uuml;ber die
+        Mieter, welche sich auch als PDF ausgeben lassen. Neue Mieter k&ouml;nen erfasst werden oder bestehende Mieter 
+        k&ouml;nnen bearbeitet oder gel&ouml;scht werden. Ausserdem wird ein Mieterspiegel gef&uuml;hrt.
         <br/>
         <br/>
         <b> Wohnungen </b> <br/>
-        (kurze Erklarung) <br/>
+        Unter dem Men&uuml;punkt Wohnungen hat es eine Wohnungsliste mit den aktuellen Wohnungen die vermietet werden,
+        welche sich als PDF ausgeben lassen. Neue Wohnungen k&ouml;nen erfasst werden oder bestehende Wohnungen 
+        k&ouml;nnen bearbeitet oder gel&ouml;scht werden. 
+        <br/>
         <br/>
         <b> Vertr&auml;ge </b> <br/>
-        (kurze Erklarung) </br>
+        Unter dem Men&uuml;punkt Vertr&auml;ge hat es eine &Uuml;bersicht der Mietvertr&auml;ge,
+        welche sich als PDF ausgeben lassen. Neue Mietvertr&auml;ge k&ouml;nen erfasst werden 
+        oder bestehende Mietvertr&auml;ge k&ouml;nnen bearbeitet oder gel&ouml;scht werden. Bezahlte Mieten 
+        k&ouml;nnen unter dem Men&uuml;punkt \"Mieteingang buchen\" an den bestehende Mietvertrag gebucht werden.
+        <br/>
         <br/>
         <b> Rechnungen </b> <br/>
-        (kurze Erklarung) <br/>
+        Unter dem Men&uuml;punkt Rechnungen hat es eine &Uuml;bersicht der erfassten Rechnungen,
+        welche sich als PDF ausgeben lassen. Neue Rechnungen k&ouml;nen erfasst werden 
+        oder bestehende Rechnungen k&ouml;nnen bearbeitet oder gel&ouml;scht werden. Unter dem Men&uuml;punkt 
+        \"Jahresrechnung\" sieht man die Kostenentwicklung. <br/>
         <br/>
         <b> Benutzer </b> <br/>
-        Unter dem Me&uuml;punkt Benutzer gibt es eine &Uuml;bersicht der aktuellen Benutzer,
-        die das Online-Verwaltungstool verwenden. Neue Benutzer k&ouml;nnen erfasst oder
-        bereits bestehene Benutzer k&ouml;nen gel&ouml;scht werden. Zudem kann das eingene Passwort
-        ge&auml;ndert werden oder man kann sich das eigene Passwort an eine gew&uuml;nschte E-Mail-Adresse
-        zusenden. <br/>
+        Unter dem Men&uuml;punkt Benutzer gibt es eine &Uuml;bersicht der aktuellen Benutzer,
+        die das Online-Verwaltungstool verwenden (PDF-Ausgabe ebenfalls m&ouml;glich). 
+        Neue Benutzer k&ouml;nnen erfasst werden oder bereits bestehene Benutzer k&ouml;nen gel&ouml;scht werden. <br/>
     </p>
 </article>
 ";
